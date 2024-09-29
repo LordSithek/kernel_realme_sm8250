9cb569b265dd9e7bfebd2e184930aa539dd43773
Lord Sithek <sithek88@gmail.com>
2024-09-29 08:58:32 +0200

    remove Image and dtbo.img after zipping

--- zip.sh
+++ zip.sh
@@ -1,7 +1,8 @@
 cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
-
 cd anykernel && zip -r Bigshot-KSU-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-KSU-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
+
+rm anykernel/Image && rm anykernel/dtbo.img
